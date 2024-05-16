e70c880818e0a7bcb158a107d17412c634f150e2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-05-16 12:17:02 +0530

    change event

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ChangeEvent } from "react";
+
 export default function Home() {
   const uploadFile = (e: ChangeEvent<HTMLInputElement>) => {
     const file: File | null | undefined = e.target.files?.[0];
